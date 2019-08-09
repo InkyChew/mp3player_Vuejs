8dc2256ecaa94ff1792181a49bdb6e92f7ef8725
InkyChew <B10623019@yuntech.edu.tw>
2019-08-09 14:28:48 +0800

    Updates

---
+++ precache-manifest.e688621035e9571d65fbc04513749b87.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "c35fb5a72c192ec70585de9137ec78de",
+    "url": "media/Happier.c35fb5a7.mp3"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "315afb2afcf51034a9ca0ff122347bf3",
+    "url": "img/multiply.315afb2a.png"
+  },
+  {
+    "revision": "523044e96e3d30b6a6c6",
+    "url": "css/about.4d36234d.css"
+  },
+  {
+    "revision": "894614258dd88a6a48bb",
+    "url": "js/app.0a6db1b8.js"
+  },
+  {
+    "revision": "919b5cb587ab93837db1",
+    "url": "js/chunk-vendors.979ca158.js"
+  },
+  {
+    "revision": "2c634007c8504372d3fbd7770fff34fd",
+    "url": "img/plus.2c634007.png"
+  },
+  {
+    "revision": "2c684d96ec461408119f328320b1f485",
+    "url": "media/Dive.2c684d96.mp3"
+  },
+  {
+    "revision": "f0da39b9f413ddf8a969cdaac3ff0cbe",
+    "url": "media/How Would You Feel (Paean).f0da39b9.mp3"
+  },
+  {
+    "revision": "82af1a95e2642525de7b",
+    "url": "js/Home.bfa20e0c.js"
+  },
+  {
+    "revision": "523044e96e3d30b6a6c6",
+    "url": "js/about.33cef715.js"
+  },
+  {
+    "revision": "637a8369bc9a4091c13559b8846c69ba",
+    "url": "img/Ed Sheeran.637a8369.jpg"
+  },
+  {
+    "revision": "4a71ef14d14faa4525580f329196b362",
+    "url": "media/Castle On The Hill.4a71ef14.mp3"
+  },
+  {
+    "revision": "f146111641de4ed32ab4215fcd4d40fe",
+    "url": "img/divide.f1461116.png"
+  },
+  {
+    "revision": "dfd98e2c0251a2984382704c52f7eb46",
+    "url": "media/Perfect.dfd98e2c.mp3"
+  },
+  {
+    "revision": "e5a758161a7f88e7ab2ab4840cd96b0a",
+    "url": "img/new release.e5a75816.jpg"
+  },
+  {
+    "revision": "959606ade0c6f4c4aabd764a5bb8414d",
+    "url": "media/Shape Of You.959606ad.mp3"
+  },
+  {
+    "revision": "8a7cdb04bb5221c2f777ca0dd04c34b2",
+    "url": "index.html"
+  },
+  {
+    "revision": "82af1a95e2642525de7b",
+    "url": "css/Home.0beb1d08.css"
+  }
+];
